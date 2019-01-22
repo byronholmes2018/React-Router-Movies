57203702d4ff88924ffea8dc9b598473ec01f888
byronholmes2018 <lshopeful2018@mgmail.com>
2019-01-22 16:53:03 -0600

    finished MVP

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
+        <Route exact path = "/" component = {MovieList} />
+        <Route path = "/movies/:id" component = {Movie} />
       </div>
     );
   }
